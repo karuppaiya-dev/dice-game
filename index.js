ae034b7aa23cc0bb43ac1333fb9c33f94b992971
Karuppaiya murugan <karuppaiya.fullstackdev@gmail.com>
2026-01-10 13:44:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
-randomNumber1 = Math.floor(Math.random() * 6) + 1;
-randomNumber2 = Math.floor(Math.random() * 6) + 1;
-document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
-document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+document.getElementById("rollBtn").addEventListener("click", rollDice);
 
-if (randomNumber1 > randomNumber2) {
-    document.querySelector(".result").innerHTML = "Player 1 Wins!🚩";
+function rollDice() {
+
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector(".result").innerHTML = "🚩 Player 1 Wins!";
+  } 
+  else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".result").innerHTML = "🚩 Player 2 Wins!";
+  } 
+  else {
+    document.querySelector(".result").innerHTML = "It's a Draw!";
+  }
 }
-else if (randomNumber1 < randomNumber2) {
-    document.querySelector(".result").innerHTML = "Player 2 Wins!🚩";
-}
-else {
-    document.querySelector(".result").innerHTML = "Draw!";
-} 
