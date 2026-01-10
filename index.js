55c01d00dc4816488652f8862abb3c6c6dd5990d
Karuppaiya murugan <karuppaiya.fullstackdev@gmail.com>
2026-01-10 11:14:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+randomNumber1 = Math.floor(Math.random() * 6) + 1;
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector(".result").innerHTML = "Player 1 Wins!🚩";
+}
+else if (randomNumber1 < randomNumber2) {
+    document.querySelector(".result").innerHTML = "Player 2 Wins!🚩";
+}
+else {
+    document.querySelector(".result").innerHTML = "Draw!";
+} 
